18ed9af7ec450d721c9fb86daeca6b745163016a
Cheng-Chen-NYU <cc6858@nyu.edu>
2021-12-06 23:28:52 -0500

    as-whole

--- hpc.sh
+++ hpc.sh
@@ -10,5 +10,5 @@
 #SBATCH --mail-user=cc6858@nyu.edu
 
 cd /scratch/$USER/EvolvingPageRank || exit
-python main.py --dataset 'as-733' --probing_nodes_num 10 --sequence_length 733 --fig_path ./imgs/as_10.png
-#python main.py --dataset 'as-caida' --probing_nodes_num 100 --sequence_length 122 --fig_path ./imgs/caida_100.png
+#python main.py --dataset 'as-733' --probing_nodes_num 10 --sequence_length 733 --fig_path ./imgs/as_10.png
+python main.py --dataset 'as-caida' --probing_nodes_num 100 --sequence_length 122 --fig_path ./imgs/caida_100.png
